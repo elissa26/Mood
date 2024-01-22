86d7726a6bd028e3a3c8f82ab07b496cc8744303
elissa26 <62772808+elissa26@users.noreply.github.com>
2024-01-22 23:51:10 +0100

    Delete all

--- firmware/src/interface_callbacks.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <functional>
-
-#include "proto_gen/smartknob.pb.h"
-
-typedef std::function<void(PB_SmartKnobConfig&)> ConfigCallback;
-typedef std::function<void(void)> MotorCalibrationCallback;
--- firmware/src/motors/motor_config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if MOTOR_WANZHIDA_ONCE_TOP
-#include "motors/wanzhida_once_top.h"
-#elif MOTOR_MAD2804
-#include "motors/mad2804.h"
-#else
-#error "No motor configuration specified!"
-#endif
