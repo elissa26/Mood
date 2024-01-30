8a6626b2c3c77bec4db77b826a7b4e898d0a7adf
elissa26 <62772808+elissa26@users.noreply.github.com>
2024-01-30 21:38:14 +0100

    Delete all

--- firmware/src/util.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-template <typename T> T CLAMP(const T& value, const T& low, const T& high) 
-{
-  return value < low ? low : (value > high ? high : value); 
-}
--- firmware/src/welcome_image.h
+++
@@ -1,7 +0,0 @@
-#ifndef WELCOME_IMAGE_H
-#define WELCOME_IMAGE_H
-#include "lvgl.h"
-
-extern const lv_img_dsc_t welcome_image;
-
-#endif // WELCOME_IMAGE_H
