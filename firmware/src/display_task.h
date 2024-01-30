cab854e9a0123ebe122729b14478d95ae41b6be0
elissa26 <62772808+elissa26@users.noreply.github.com>
2024-01-30 21:43:36 +0100

    Lvgl with master branch (only welcome screen)

---
+++ firmware/src/display_task.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#if SK_DISPLAY
+
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include "lvgl.h"
+
+#include "logger.h"
+#include "proto_gen/smartknob.pb.h"
+#include "task.h"
+
+#define DISP_BUF_SIZE  (TFT_WIDTH * 10)
+
+class DisplayTask : public Task<DisplayTask> {
+    friend class Task<DisplayTask>; // Allow base Task to invoke protected run()
+
+    public:
+        DisplayTask(const uint8_t task_core);
+        ~DisplayTask();
+
+        QueueHandle_t getKnobStateQueue();
+
+        void setBrightness(uint16_t brightness);
+        void setLogger(Logger* logger);
+
+    protected:
+        void run();
+
+    private:
+        TFT_eSPI tft_ = TFT_eSPI();
+        /** Full-size sprite used as a framebuffer */
+        TFT_eSprite spr_ = TFT_eSprite(&tft_);
+
+        lv_obj_t * screen;
+        lv_obj_t * label_cur_pos;
+        lv_obj_t * label_desc;
+        lv_obj_t * arc;
+        lv_obj_t * gauge;
+        lv_obj_t * roller;
+        lv_obj_t * line_left_bound;
+        lv_obj_t * line_right_bound;
+        lv_obj_t * arc_dot;
+
+        QueueHandle_t knob_state_queue_;
+
+        PB_SmartKnobState state_;
+        SemaphoreHandle_t mutex_;
+        uint16_t brightness_;
+        Logger* logger_;
+        void log(const char* msg);
+};
+
+#else
+
+class DisplayTask {};
+
+#endif
